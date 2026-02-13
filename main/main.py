c46f1cbd6d3b0f6ab383137337d0059edd03c66d
ben1957mit <ben1957mit@gmail.com>
2026-02-13 17:36:03 -0600

    Delete main/main.py

--- main/main.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Trailer Optimization Suite", layout="wide")
-
-st.title("Trailer Optimization Suite")
-st.write("Choose a tool from the sidebar to begin.")

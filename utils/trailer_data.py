481c19e4327c3eb4baf378257e1cb4c73fc301c8
ben1957mit <ben1957mit@gmail.com>
2026-02-13 17:04:21 -0600

    Create trailer_data.py

---
+++ utils/trailer_data.py
@@ -0,0 +1,20 @@
+TRAILERS = {
+    "53ft Dry Van": {
+        "length": 53,
+        "width": 102,
+        "height": 110,
+        "max_weight": 45000
+    },
+    "48ft Dry Van": {
+        "length": 48,
+        "width": 102,
+        "height": 110,
+        "max_weight": 42000
+    },
+    "28ft Pup": {
+        "length": 28,
+        "width": 102,
+        "height": 110,
+        "max_weight": 22000
+    }
+}
